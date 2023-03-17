d37b2e181dd03ed4f3a8b4903fd22105f03511c8
fedechaves <federicochavesf@gmail.com>
2023-03-17 10:09:32 -0300

    feat: hotel controllers done

---
+++ api/controllers/hotel.js
@@ -0,0 +1,72 @@
+import express from "express";
+import Hotel from "../models/Hotel.js";
+import { createError } from "../utils/error.js";
+
+const router = express.Router();
+
+//CREATE
+export const createHotel = async (req, res, next) => {
+    
+    const newHotel = new Hotel(req.body);
+
+    try {
+        const savedHotel = await newHotel.save();
+        res.status(200).json(savedHotel)
+
+    }catch(err) {
+        next(err);
+    }
+}
+
+//UPDATE
+export const updateHotel = async (req, res, next) => {
+
+    try {
+        const updatedHotel = await Hotel.findByIdAndUpdate(req.params.id, 
+            { $set: req.body },
+            { new: true });
+        res.status(200).json(updatedHotel);
+
+    }catch(err) {
+        next(err);
+    }
+};
+
+//DELETE
+
+export const deleteHotel = async (req, res, next) => {
+
+    try {
+        await Hotel.findByIdAndDelete(req.params.id);
+        res.status(200).json("Hotel has been deleted!");
+
+    }catch(err) {
+        next(err);
+    }
+};
+
+//GET
+export const getHotel = async (req, res) => {
+
+    try {
+        const hotel = await Hotel.findById(req.params.id);
+        res.status(200).json(hotel);
+
+    }catch(err) {
+        next(err);
+    }
+};
+
+//GET ALL
+export const getAllHotels = async (req, res) => {
+
+    try {
+        const hotels = await Hotel.find();
+        res.status(200).json(hotels);
+
+    }catch(err) {
+        next(err);
+    }
+};
+
+export default router
