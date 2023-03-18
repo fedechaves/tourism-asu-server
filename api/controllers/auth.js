72c98f13b0b9531e41a545e2d78caabb6419a458
fedechaves <federicochavesf@gmail.com>
2023-03-18 18:37:14 -0300

    feat: authentication done

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -1,7 +1,7 @@
 import User from "../models/User.js";
 import bcrypt from "bcryptjs";
-import mongoose from "mongoose";
 import { createError } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 export const register = async (req, res, next) => {
     try{
@@ -26,16 +26,20 @@ export const login = async (req, res, next) => {
     try{
 
         const user = await User.findOne({ username: req.body.username })
-        if(!user) {
-            return next(createError(404, "User not Found"))
-        }
-        const isPasswordCorrect = await bcrypt.compare(req.body.password, user.password)
-        if(!isPasswordCorrect) {
-            return next(createError(400, "password or username not matching"))
-        }
+        if(!user) return next(createError(404, "User not Found"));
+        
+        const isPasswordCorrect = await bcrypt.compare(req.body.password, user.password);
+        if(!isPasswordCorrect) 
+            return next(createError(400, "password or username not matching"));
+        
+        const token = jwt.sign({ id: user._id, isAdmin: user.isAdmin }, process.env.JWT_TOKEN) 
 
         const {password, isAdmin, ...otherDetails} = user._doc;
-        res.status(200).send({...otherDetails})
+        res.cookie("access_token", token, {
+            httpOnly: true,
+        })
+        .status(200)
+        .send({...otherDetails})
     }catch(err) {
         next(err)
     }
